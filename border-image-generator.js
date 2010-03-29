6cbc93799827e14cee5c230435cd2d4868180b18
Kevin Decker <kdecker@doyle.local>
2010-03-28 19:21:11 -0500

    Update data model to use a single state variable

--- border-image-generator.js
+++ border-image-generator.js
@@ -12,41 +12,43 @@ $(document).ready(function() {
         dividers = $(".divider"),
         sliders = $(".slider"),
 
-        borderWidth = [0, 0, 0, 0],
-        imageOffset = [0, 0, 0, 0],
-        scaleFactor = 3;
+        state = {
+            borderWidth: [0, 0, 0, 0],
+            imageOffset: [0, 0, 0, 0],
+            scaleFactor: 3
+        };
 
     var sliderMap = {
-        imageTop: { array: imageOffset, index: 0 },
-        imageRight: { array: imageOffset, index: 1 },
-        imageBottom: { array: imageOffset, index: 2 },
-        imageLeft: { array: imageOffset, index: 3 },
+        imageTop: { array: state.imageOffset, index: 0 },
+        imageRight: { array: state.imageOffset, index: 1 },
+        imageBottom: { array: state.imageOffset, index: 2 },
+        imageLeft: { array: state.imageOffset, index: 3 },
 
-        borderTop: { array: borderWidth, index: 0 },
-        borderRight: { array: borderWidth, index: 1 },
-        borderBottom: { array: borderWidth, index: 2 },
-        borderLeft: { array: borderWidth, index: 3 },
+        borderTop: { array: state.borderWidth, index: 0 },
+        borderRight: { array: state.borderWidth, index: 1 },
+        borderBottom: { array: state.borderWidth, index: 2 },
+        borderLeft: { array: state.borderWidth, index: 3 },
     }, dividerMap = {
         dividerTop: {
-            setValue: function(el) { imageOffset[0] = calcPixels($(el).position().top) },
-            updatePos: function(el) { $(el).css("top", imageOffset[0]*scaleFactor); }
+            setValue: function(el) { state.imageOffset[0] = calcPixels($(el).position().top) },
+            updatePos: function(el) { $(el).css("top", state.imageOffset[0]*state.scaleFactor); }
         },
         dividerRight: {
-            setValue: function(el) { imageOffset[1] = calcPixels(editorEl.innerWidth() - $(el).position().left - 2) },
-            updatePos: function(el) { $(el).css("left", (editorEl.innerWidth() - imageOffset[1]*scaleFactor) + 2); }
+            setValue: function(el) { state.imageOffset[1] = calcPixels(editorEl.innerWidth() - $(el).position().left - 2) },
+            updatePos: function(el) { $(el).css("left", (editorEl.innerWidth() - state.imageOffset[1]*state.scaleFactor) + 2); }
         },
         dividerBottom: {
-            setValue: function(el) { imageOffset[2] = calcPixels(editorEl.innerHeight() - $(el).position().top - 2) },
-            updatePos: function(el) { $(el).css("top", (editorEl.innerHeight() - imageOffset[2]*scaleFactor) + 2); }
+            setValue: function(el) { state.imageOffset[2] = calcPixels(editorEl.innerHeight() - $(el).position().top - 2) },
+            updatePos: function(el) { $(el).css("top", (editorEl.innerHeight() - state.imageOffset[2]*state.scaleFactor) + 2); }
         },
         dividerLeft: {
-            setValue: function(el) { imageOffset[3] = calcPixels($(el).position().left) },
-            updatePos: function(el) { $(el).css("left", imageOffset[3]*scaleFactor); }
+            setValue: function(el) { state.imageOffset[3] = calcPixels($(el).position().left) },
+            updatePos: function(el) { $(el).css("left", state.imageOffset[3]*state.scaleFactor); }
         },
     }
     
     function calcPixels(pos) {
-        return (pos / scaleFactor) | 0;
+        return (pos / state.scaleFactor) | 0;
     }
     function updateSliders() {
         $(".image-offset").each(function(index, el) {
@@ -61,8 +63,8 @@ $(document).ready(function() {
     }
     function updateCSS() {
         var img = "url(" + pathToImage.val() + ")",
-            borderImage = img + " " + imageOffset.join(" "),
-            borderWidthStr = borderWidth.join("px ") + "px",
+            borderImage = img + " " + state.imageOffset.join(" "),
+            borderWidthStr = state.borderWidth.join("px ") + "px",
             style = "border-width: " + borderWidthStr + ";\n"
                 + "-moz-border-image: " + borderImage + ";\n"
                 + "-webkit-border-image: " + borderImage + ";\n"
@@ -99,14 +101,15 @@ $(document).ready(function() {
 
     imageEl.load(function() {
          var img = this,
-            width = img.natualWidth*scaleFactor,
-            height = img.naturalHeight*scaleFactor;
+            width = img.natualWidth*state.scaleFactor,
+            height = img.naturalHeight*state.scaleFactor;
 
         $("#editorEl, #imageEl").width(width).height(height);
         updateCSS();
     })
     pathToImage.change(function(event) {
-        imageEl[0].src = pathToImage.val();
+        state.src = pathToImage.val();
+        imageEl[0].src = state.src;
     });
 
     pathToImage.change();
